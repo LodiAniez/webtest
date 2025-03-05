a0562cb69f6837224b3485fcab1fed4016d93c72
aniezlouie <lodianiez@gmail.com>
2025-03-05 12:37:36 +0800

    Implement recalculation on scroll

--- src/screens/pagination/index.tsx
+++ src/screens/pagination/index.tsx
@@ -1,124 +1,141 @@
-import Colors from '@/common/colors';
-import Images from '@/common/images';
-import Modal from '@/components/Modal';
-import TextInput from '@/components/TextInput';
-import useCollectionObserver from '@/hooks/useCollectionObserver';
-import useKeyListener from '@/hooks/useKeyListener';
-import { useAuth } from '@/services/context/AuthContext'
-import { googleSignIn } from '@/services/firebase/auth';
-import { createDocument, createId } from '@/services/firebase/firestore';
-import { MessageType } from '@/types';
-import { sanitizeString } from '@/utils/functions';
-import { TSDate, UTCDate } from '@/utils/variables';
-import React, { useCallback, useEffect, useState } from 'react'
-import MessageRow from './components/MessageRow';
-import { signOut } from 'firebase/auth';
-import { auth } from '@/services/firebase/config';
-import { orderBy } from 'firebase/firestore';
+import Colors from "@/common/colors";
+import Images from "@/common/images";
+import Modal from "@/components/Modal";
+import TextInput from "@/components/TextInput";
+import useCollectionObserver from "@/hooks/useCollectionObserver";
+import useKeyListener from "@/hooks/useKeyListener";
+import { useAuth } from "@/services/context/AuthContext";
+import { googleSignIn } from "@/services/firebase/auth";
+import { createDocument, createId } from "@/services/firebase/firestore";
+import { MessageType } from "@/types";
+import { sanitizeString } from "@/utils/functions";
+import { TSDate, UTCDate } from "@/utils/variables";
+import React, { useCallback, useEffect, useState } from "react";
+import MessageRow from "./components/MessageRow";
+import { signOut } from "firebase/auth";
+import { auth } from "@/services/firebase/config";
+import { orderBy, limit, startAfter } from "firebase/firestore";
 
 const Pagination = () => {
-    const {
-        Auth,
-        logout,
-    } = useAuth();
-    const [message, setMessage] = useState("")
-    const [messageError, setMessageError] = useState(false)
+  const { Auth, logout } = useAuth();
+  const [message, setMessage] = useState("");
+  const [messageError, setMessageError] = useState(false);
+  const [messageLimit, setMessageLimit] = useState(20);
 
+  const Messages = useCollectionObserver<MessageType>({
+    Collection: "messages",
+    Condition: [orderBy("createdAt", "desc"), limit(messageLimit)],
+    Dependencies: [messageLimit],
+  });
 
-    const Messages = useCollectionObserver<MessageType>({
-        Collection: "messages",
-        Condition: [
-            orderBy("createdAt", "desc")
-        ]
-    })
-    const sendMessage = useCallback(async () => {
-        if (!Auth) return;
-        const sanitized = sanitizeString(message);
-        if (typeof message !== "string" || sanitized.trim() === "") return setMessageError(true);
-        await createDocument<MessageType>({
-            Collection: "messages",
-            Data: {
-                authorId: Auth.uid,
-                createdAt: TSDate(),
-                id: createId("messages"),
-                message: sanitized
-            }
-        });
-        setMessage("");
-    }, [message, Auth]);
+  const handleScroll = useCallback(
+    (e: React.UIEvent<HTMLDivElement>) => {
+      const scrollTop = e.currentTarget.scrollTop;
+      const maxNegativeScroll =
+        e.currentTarget.scrollHeight - e.currentTarget.clientHeight;
 
-    const handleSend = useCallback(async () => {
-        if (!Auth) await googleSignIn().then((response) => {
-            if (response.status !== 200) alert(response.message);
-            else sendMessage()
-        });
-        else sendMessage()
-    }, [Auth, sendMessage])
+      const isAtTop = Math.abs(scrollTop) === Math.abs(maxNegativeScroll);
 
+      const diff = messageLimit - Messages.length;
 
-    useKeyListener({
-        key: "Enter",
-        callback: handleSend,
-        dependencies: [handleSend, Auth]
+      if (isAtTop && diff === 0) {
+        setMessageLimit((prevLimit) => prevLimit + 20);
+      }
+    },
+    [messageLimit, Messages]
+  );
+
+  const sendMessage = useCallback(async () => {
+    if (!Auth) return;
+    const sanitized = sanitizeString(message);
+    if (typeof message !== "string" || sanitized.trim() === "")
+      return setMessageError(true);
+    await createDocument<MessageType>({
+      Collection: "messages",
+      Data: {
+        authorId: Auth.uid,
+        createdAt: TSDate(),
+        id: createId("messages"),
+        message: sanitized,
+      },
     });
+    setMessage("");
+  }, [message, Auth]);
 
-    return (
-        <>
+  const handleSend = useCallback(async () => {
+    if (!Auth)
+      await googleSignIn().then((response) => {
+        if (response.status !== 200) alert(response.message);
+        else sendMessage();
+      });
+    else sendMessage();
+  }, [Auth, sendMessage]);
 
-            <div
-                style={{
-                    background: Colors.black500,
-                    top: `${innerHeight / 4}px`,
-                    left: `calc(50% - 200px)`
-                }}
-                className="m-auto br-15px card text-center w-400px h-min-400px h-50vh absolute col gap-5px" >
-                <div className="col-reverse gap-3px h-100p overflow-y-scroll visible-scrollbar">
-                    {
-                        Messages.map((msg) => <MessageRow message={msg} key={msg.id} />)
-                    }
-                </div>
-                <div className="row-center">
-                    <TextInput
-                        value={message}
-                        setValue={(e) => {
-                            setMessage(e.target.value);
-                            if (messageError) setMessageError(false)
-                        }}
-                        error={messageError}
-                        inputClassName='bootstrap-input'
-                        containerClassName='m-3px w-100p'
-                    />
-                    <button
-                        onClick={handleSend}
-                        className='h-30px w-30px'
-                        style={{ background: Colors.transparent }}
-                        type="button">
-                        <img
-                            className='h-25px w-25px mr-3px'
-                            src={Images["ic_send_white"]} />
-                    </button>
-                </div>
-            </div>
+  useKeyListener({
+    key: "Enter",
+    callback: handleSend,
+    dependencies: [handleSend, Auth],
+  });
 
-            {
-                Auth ?
-                    <button
-                        style={{
-                            bottom: "100px",
-                            left: "calc(50% - 39px)",
-                            border: "1px solid " + Colors.white
-                        }}
-                        onClick={logout}
-                        className='absolute br-3px pv-5px ph-15px'
-                        type="button">
-                        Logout
-                    </button>
-                    : null
-            }
-        </>
-    )
-}
+  return (
+    <>
+      <div
+        style={{
+          background: Colors.black500,
+          top: `${innerHeight / 4}px`,
+          left: `calc(50% - 200px)`,
+        }}
+        className="m-auto br-15px card text-center w-400px h-min-400px h-50vh absolute col gap-5px"
+      >
+        <div
+          onScroll={handleScroll}
+          className="col-reverse gap-3px h-100p overflow-y-scroll visible-scrollbar"
+        >
+          {Messages.map((msg) => (
+            <MessageRow message={msg} key={msg.id} />
+          ))}
+        </div>
+        <div className="row-center">
+          <TextInput
+            value={message}
+            setValue={(e) => {
+              setMessage(e.target.value);
+              if (messageError) setMessageError(false);
+            }}
+            error={messageError}
+            inputClassName="bootstrap-input"
+            containerClassName="m-3px w-100p"
+          />
+          <button
+            onClick={handleSend}
+            className="h-30px w-30px"
+            style={{ background: Colors.transparent }}
+            type="button"
+          >
+            <img
+              className="h-25px w-25px mr-3px"
+              src={Images["ic_send_white"]}
+            />
+          </button>
+        </div>
+      </div>
 
+      {Auth ? (
+        <button
+          style={{
+            bottom: "100px",
+            left: "calc(50% - 39px)",
+            border: "1px solid " + Colors.white,
+          }}
+          onClick={logout}
+          className="absolute br-3px pv-5px ph-15px"
+          type="button"
+        >
+          Logout
+        </button>
+      ) : null}
+    </>
+  );
+};
 
-
-export default Pagination
+export default Pagination;
